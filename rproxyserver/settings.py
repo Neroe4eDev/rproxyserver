8cd1b3da6621310265f2b1fc1eee4e0af02fb7e5
Daniel Adeyemo <letroot@ehealth4everyone.cloud>
2018-02-06 12:04:32 +0100

    Update settings

--- rproxyserver/settings.py
+++ rproxyserver/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0vhp+0%dgxfdg2ndhm%d=!#ecq^myq-=ri!^3babg%afr(g(35'
+from .secret_settings import SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [127.0.0.1, localhost]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 
 # Application definition
